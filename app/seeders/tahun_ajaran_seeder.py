405eb78047bb26e87700e91e4edc86cb7ee0dce9
wendwin <wendwin12@gmail.com>
2026-05-09 23:28:18 +0700

    menyesuaikan seeder gelombang dan tahun ajaran

--- app/seeders/tahun_ajaran_seeder.py
+++ app/seeders/tahun_ajaran_seeder.py
@@ -2,13 +2,11 @@ from app.extensions import db
 from app.models.pendaftaran.tahun_ajaran import TahunAjaran
 
 def seed_tahun_ajaran():
-    data = [
-        # {"tahun_mulai": 2023, "tahun_selesai": 2024, "status": "nonaktif"},
-        # {"tahun_mulai": 2024, "tahun_selesai": 2025, "status": "nonaktif"},
-        {"tahun_mulai": 2025, "tahun_selesai": 2026, "status": "aktif"},
-    ]
+
+    data = [{"tahun_mulai": 2025,"tahun_selesai": 2026,"is_active": True},]
 
     for item in data:
+
         exists = TahunAjaran.query.filter_by(
             tahun_mulai=item["tahun_mulai"],
             tahun_selesai=item["tahun_selesai"]
@@ -18,4 +16,5 @@ def seed_tahun_ajaran():
             db.session.add(TahunAjaran(**item))
 
     db.session.commit()
+
     print("Seeder tahun ajaran berhasil")
